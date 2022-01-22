d7dab0930b6abd3cfd8ed7957ee14f281bdf0906
PierroElMito <pierroelmito@gmail.com>
2022-01-22 11:33:12 +0100

    fix link error with MSYS2

--- xmake.lua
+++ xmake.lua
@@ -3,7 +3,8 @@ add_rules("mode.debug", "mode.release")
 
 target("sc")
 	before_build(function (target)
-		os.exec("sokol-shdc --input src/shaders/shaders.glsl --output src/shaders/shaders.h --slang glsl330", sourcefile, np)
+		-- uncomment do rebuild shaders from glsl:
+		--os.exec("sokol-shdc --input src/shaders/shaders.glsl --output src/shaders/shaders.h --slang glsl330", sourcefile, np)
 	end)
 	set_kind("binary")
 	add_includedirs("src", "src/sc", "src/ext")
@@ -12,7 +13,7 @@ target("sc")
 	add_files("src/**.cpp")
 	add_defines("USE_SHADER_PIPELINE=1")
 	if is_plat("mingw") then
-		add_links("opengl32", "glfw3")
+		add_links("opengl32", "ole32", "glfw3")
 	else
 		add_links("GL", "glfw", "pthread", "asound")
 	end
