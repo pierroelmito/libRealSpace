b72d832a5b695d58c05701e8449c34806f5f450f
pierro <pierroelmito@gmail.com>
2025-10-25 00:14:46 +0200

    clouds wip

--- data/shaders/common.glsl
+++ data/shaders/common.glsl
@@ -27,16 +27,44 @@ vec3 baseSkyColor(vec3 dir) {
 	return vec3(mix(vec3(1.0, 1.0, 1.0), vec3(0.5, 0.7, 1.0), t));
 }
 
-#if 0
+#if 1
+
+vec4 traceClouds(float hb, float ht, vec3 from, vec3 to, vec3 dir)
+{
+	float res = 0.0;
+	float c = 1 - exp(-abs((ht - hb) / dir.y));
+	for (int i = 0; i < 32; i++) {
+		float r0 = float(i) / 15.0;
+		vec3 pos = mix(to, from, r0);
+		float r1 = (pos.y - hb) / (ht - hb);
+		float th = 0.1 + 0.8 * pow(r1 * 2 - 1, 2);
+		float d = smoothstep(th - 0.2, th + 0.2, cloudTest(pos.xz));
+		res += c * d / 16.0;
+	}
+	return vec4(min(1, res), 0, 0, 0);
+	//return 0.5 * vec4(cloudTest(from.xz), 0, 0, 1) + 0.5 * vec4(cloudTest(to.xz), 0, 0, 1);
+}
 
 vec4 makeSkyColor(vec4 sky, vec3 campos, vec3 dir, vec3 wpos) {
-	float ht = cloudH + 1.0;
-	float hb = cloudH - 1.0;
-	if (campos.y < hb && dir.y < 0.0)
+	float ht = cloudH + 8.0;
+	float hb = cloudH - 8.0;
+	if (dir.y < 0.0 && (campos.y < hb || wpos.y > ht))
 		return sky;
-	if (campos.y > ht && dir.y > 0.0)
+	if (dir.y > 0.0 && (campos.y > ht || wpos.y < hb))
 		return sky;
-	return vec4(1, 0, 0, 1);
+	vec3 from, to;
+	if (dir.y < 0) {
+		from = campos - (dir * ((campos.y - ht) / dir.y));
+		to = campos - (dir * ((campos.y - hb) / dir.y));
+	} else {
+		from = campos - (dir * ((campos.y - hb) / dir.y));
+		to = campos - (dir * ((campos.y - ht) / dir.y));
+	}
+	//if ((campos.y - to.y) * (campos.y - from.y) < 0.0)
+	//	from = campos;
+	if ((wpos.y - to.y) * (wpos.y - from.y) < 0.0)
+		to = wpos;
+	return vec4(vec3(1, 1, 0), traceClouds(hb, ht, from, to, dir).x);
 }
 
 vec4 sceneSkyColor(float z, vec3 campos, vec3 dir, vec3 wpos) {
